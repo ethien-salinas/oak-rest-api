6d1ded30202f52800430bca1db5a3db2e6f9454c
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-09-30 17:32:08 -0500

    [add] User definition

---
+++ db/Users.ts
@@ -0,0 +1,9 @@
+import IUser from "../model/IUser.ts";
+
+let Users: Array<IUser> = [
+  { username: "Ethien", password: "qwerty" },
+  { username: "Pablo", password: "asdfgh" },
+  { username: "Lisandro", password: "zxcvbn" },
+];
+
+export default Users;
